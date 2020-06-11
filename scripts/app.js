5eaffe489a26f943ada0f47e4a70e4d1115f89c7
Darius Dinkins <dtdinkins@my365.bellevue.edu>
2020-06-11 10:56:23 -0400

    Added True or False distractors

--- scripts/app.js
+++ scripts/app.js
@@ -13,14 +13,14 @@ var viewModel = function() {
     //array of questions setup as question, [answers], question number, correct answer
       new questionViewModel("What is JavaScript?", ["A.	JavaScript is a programming/scripting language that allows the user to manipulate the web", "B.	JavaScript is a coffee project for teaching a foreign language", "C.	JavaScript is a new technological achievement that is being researched by NASA", "D.	JavaScript is a form of music practiced by the ancients of history"], 1, "Answer 4"),
       new questionViewModel("What are functions?", ["A.	A block of JavaScript code that is defined once but may be executed, or invoked, any number of times.", "B.	A block of JavaScript code that is mentioned in passing via secret meetings", "C.	A block of JavaScript code that is used to create disruptions in the system", "D.	A block of JavaScript code that is known as “the coffee break”"], 2, "Answer 6"),
-      new questionViewModel("True or False: Arrays are list-like objects whose prototype has methods to perform traversal and mutation operations?", ["A.	True", "B.	False", "", ""], 3, "Answer 9"),
+      new questionViewModel("True or False: Arrays are list-like objects whose prototype has methods to perform traversal and mutation operations?", ["A.	True", "B.	False", "C. Both", "D. Neither"], 3, "Answer 9"),
       new questionViewModel("Select an example of an array", ["A. function array(design) \n \n { return design*design; }", "B.	var array = 1", "C.	var array = ‘array’", "D.	var array = [1, 2, 3]"], 4, "Answer 15"),
       new questionViewModel("A variable can be made up of…", ["Strings and headsets", "B.	Numbers and keys", "C.	Strings and Numbers", "D.	Keys and Headsets"], 5, "Answer 19"),
-      new questionViewModel("True or False: JavaScript code can be implemented into html using the 'script' tag.", ["A.	True", "B.	False", "", ""], 6, "Answer 24"),
+      new questionViewModel("True or False: JavaScript code can be implemented into html using the 'script' tag.", ["A.	True", "B.	False", "C. Both", "D. Neither"], 6, "Answer 24"),
       new questionViewModel("Which of the following is an object?", ["A.	var controller = “steel”", "B.	var controller = {material: “steel”}", "C.	var controller = 5", "D.	function controller(steel, rubber) {return steel + rubber}"], 7, "Answer 26"),
       new questionViewModel("8.	What is a comment in relation to JavaScript?", ["A.	Text that isn’t part of the code but can be used to explain sections of the program", "B.	Text that isn’t part of the code but can be used to improve readability", "C.	Text that isn’t part of the code but can be used to test alternative code", "D.	All of the above"], 8, "Answer 31"),
       new questionViewModel("9.	Select an example of comment syntax in relation to a JavaScript program ONLY", ["A.	'< ! - - Insert Comment Here - - >'", "B.	// Insert Comment Here", "C.	/* Insert Comment Here ! - - >", "D.	* Insert Comment Here *"], 9, "Answer 33"),
-      new questionViewModel("True or False: Any collection of strings can be used as a variable name", ["A.	True", "B.	False", "", ""], 10, "Answer 38")
+      new questionViewModel("True or False: Any collection of strings can be used as a variable name", ["A.	True", "B.	False", "C. Both", "D. Neither"], 10, "Answer 38")
       ];
       //next and previous buttons to iterate through the array of questions
   self.currentQuestion = ko.observable(self.questions[0]);
